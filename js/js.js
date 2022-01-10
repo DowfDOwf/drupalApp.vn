1bb7f7bdcb30031f42bc282b8ec89a300717323b
Le Duc <ducsieunhan101@gmail.com>
2022-01-10 16:38:04 +0700

    Fix 4

--- js/js.js
+++ js/js.js
@@ -1,34 +1,42 @@
-$('.header-top-wrapper .about-us-wrapper .slider-wrapper .owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: false,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 1
-    },
-    1000: {
-      items: 1
+$(document).ready(function () {
+  $('#owl-one').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 1
+      }
     }
-  }
+  })
 })
-// slider 2
-$('.product-wrapper .product-slider-wrapper .owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  center: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 1
-    },
-    1000: {
-      items: 5
+$(document).ready(function () {
+  // slider 2
+  $('#owl-two').owlCarousel({
+    loop: true,
+    margin: 10,
+    nav: true,
+    center: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      600: {
+        items: 1
+      },
+      1000: {
+        items: 5
+      }
     }
-  }
+  })
+  $('#owl-two .owl-nav').removeClass('disabled');
+  $('#owl-two .owl-nav').click(function () {
+    $(this).removeClass('disabled');
+  })
 })
